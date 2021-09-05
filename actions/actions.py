6affcf65ebc46663406fe7255bfc01068188252f
Domenico Trotta <43494171+domenico97@users.noreply.github.com>
2021-09-05 11:07:20 +0200

    update

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,14 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+import json
 #
 #
 # class ActionHelloWorld(Action):
@@ -26,22 +30,23 @@
 #
 #         return []
 
-import datetime as dt 
-from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
+class customAction(Action):
 
+     def name(self) -> Text:
+         return "action_hello_world"
 
-class ActionHelloWorld(Action):
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-    def name(self) -> Text:
-        return "action_show_time"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text=f"{dt.datetime.now()}")
-
-        return []
+            prova = "OK VA BENE"
+            print(prova)
+            #request = requests.get('http://api.icndb.com/jokes/random').json() #make an api call
+            #joke = request['value']['joke'] #extract a joke from returned json response
+            r = requests.post('http://127.0.0.1:5001/predict', data = {'id':'1_short_sleeve_top_3145741-64470548-2560-1440.jpg'})
+            print("SONO QUI")
+            print(r.text)
+            dispatcher.utter_message(r.text) #send the message back to the user
+           
+            return []
